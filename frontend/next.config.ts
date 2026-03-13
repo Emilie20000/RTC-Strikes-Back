4d97027a289d0957e08a24411e481db973fa2204
Raigaa <ethan1.croenne@epitech.eu>
2026-03-13 15:12:36 +0100

    feat(tauri): init tauri using base source code

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: process.env.IS_TAURI === 'true' ? "export" : "standalone",
   reactCompiler: true,
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080',
