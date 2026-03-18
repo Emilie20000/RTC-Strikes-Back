18302bc455c15a7f09c4679716e7a8e20b9ce68b
Raigaa <ethan1.croenne@epitech.eu>
2026-03-18 13:04:25 +0100

    fix(launcher): adapt launcher for --dev and--prod

--- launcher.sh
+++ launcher.sh
@@ -3,18 +3,27 @@
 MODE=$1
 
 if [ "$MODE" == "--prod" ]; then
-    echo "🚀 Mode PRODUCTION"
-    echo "Nettoyage des services Docker..."
-    docker compose down
+    docker compose down -v
+    echo "🚀 Mode PRODUCTION (Tauri Build & Launch)"
     
-    echo "Lancement des services Backend (Prod)..."
-    docker compose up --build -d backend pgadmin redis frontend-web
+    rm -f frontend/src-tauri/target/release/bundle/appimage/*.AppImage
     
     echo "Lancement du Build Tauri..."
     cd frontend
     npm install
-    npm run tauri build
-    echo "✅ Build terminé. AppImage disponible dans frontend/src-tauri/target/release/bundle/appimage/"
+    IS_TAURI=true npm run tauri build
+    
+    APP_IMAGE=$(find src-tauri/target/release/bundle/appimage -name "*.AppImage" | head -n 1)
+    
+    if [ -n "$APP_IMAGE" ]; then
+        echo "✅ Build terminé : $APP_IMAGE"
+        echo "🚀 Attribution des permissions et lancement..."
+        chmod +x "$APP_IMAGE"
+        ./"$APP_IMAGE"
+    else
+        echo "❌ Erreur : AppImage non trouvé."
+        exit 1
+    fi
 
 elif [ "$MODE" == "--dev" ]; then
     echo "🛠️ Mode DEVELOPMENT"
@@ -24,10 +33,10 @@ elif [ "$MODE" == "--dev" ]; then
     echo "Lancement des services Backend & Frontend (Dev)..."
     docker compose up --build -d backend pgadmin redis frontend-dev
     
-    echo "Lancement de Tauri en mode Dev..."
-    cd frontend
-    npm install
-    npm run tauri dev
+    # echo "Lancement de Tauri en mode Dev..."
+    # cd frontend
+    # npm install
+    # npm run tauri dev
 
 else
     echo "Usage: ./launcher.sh [--dev | --prod]"
